41658c8abcc88da0b1c06cb495846f1aac144f10
MattFerreira18 <matheusferreira.dev@gmail.com>
2023-02-18 18:53:57 -0300

    refactor(http-post-client): change to receive a obj as param

--- src/domain/data/test/mock-http-client.ts
+++ src/domain/data/test/mock-http-client.ts
@@ -1,9 +1,9 @@
-import type { HttpPostClient } from '../protocols/http/http-post-client';
+import type { HttpPostClient, HttpPostParams } from '../protocols/http/http-post-client';
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
 
-  async post(url: string): Promise<void> {
+  async post({ url }: HttpPostParams): Promise<void> {
     this.url = url;
     return Promise.resolve();
   }
