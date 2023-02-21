fe2da99edced153aa1382c1ffb3b4445f466eac6
MattFerreira18 <matheusferreira.dev@gmail.com>
2023-02-20 21:33:01 -0300

    refactor: create SUT fn

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -6,9 +6,13 @@ jest.mock('axios');
 
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const makeSut = (): AxiosHttpClient => {
+  return new AxiosHttpClient();
+};
+
 describe('AxiosHttpClient', () => {
   it('should call axios with correct URL', async () => {
-    const sut = new AxiosHttpClient();
+    const sut = makeSut();
     const url = faker.internet.url();
 
     await sut.post({ url });
