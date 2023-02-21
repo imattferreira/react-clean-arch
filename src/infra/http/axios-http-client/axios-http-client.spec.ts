5a44ef02a89573a5ab23d3aa7626659d457ce0a4
MattFerreira18 <matheusferreira.dev@gmail.com>
2023-02-20 21:40:50 -0300

    test: unify tests

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import { AxiosHttpClient } from './axios-http-client';
+import { HttpPostParams } from '@/data/protocols/http';
 
 jest.mock('axios');
 
@@ -10,13 +11,18 @@ const makeSut = (): AxiosHttpClient => {
   return new AxiosHttpClient();
 };
 
+const mockPostRequest = (): HttpPostParams<unknown> => ({
+  url: faker.internet.url(),
+  body: JSON.parse(faker.datatype.json())
+});
+
 describe('AxiosHttpClient', () => {
-  it('should call axios with correct URL and HTTP verb', async () => {
+  it('should call axios with correct URL,TTP verb and body', async () => {
     const sut = makeSut();
-    const url = faker.internet.url();
+    const { url, body } = mockPostRequest();
 
-    await sut.post({ url });
+    await sut.post({ url, body });
 
-    expect(mockedAxios.post).toHaveBeenCalledWith(url);
+    expect(mockedAxios.post).toHaveBeenCalledWith(url, body);
   });
 });
